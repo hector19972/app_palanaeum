0447cc561cc89b3ae210e55709bfd56d458cd73e
Hector <nandohector1@gmail.com>
2020-07-06 10:53:56 -0500

    subir a heroku iniciando app

---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, SimpleChanges } from '@angular/core';
+import { UsuariosService } from './services/usuarios.service';
+import { Usuario } from './models/policy.model';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'palanaeum-angular';
+
+  constructor(private servUsrs: UsuariosService) { }
+
+  artistas: any[] = [];
+  bandera: boolean;
+  usuarioLogueado: Usuario = {
+    uid: '',
+    nombre: '',
+    ApPaterno: '',
+    ApMaterno: '',
+    correo: '',
+    contraseña: '',
+    rol:''
+  };
+  usuario: Usuario = {
+    uid: '',
+    nombre: '',
+    ApPaterno: '',
+    ApMaterno: '',
+    correo: '',
+    contraseña: '',
+    rol:''
+  };
+
+  ngOnInit(): void {
+    /*
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+        this.usuario = this.servUsrs.optenerDatos(uid);
+        console.log('1  ', this.usuario);
+        console.log('2  ', this.servUsrs.optenerDatos(uid));
+        //return usuario;
+        this.usuarioLogueado=this.usuario;
+        console.log('inicio componentes', this.usuarioLogueado);
+      } else {
+        // User is signed out.
+        // ...
+      }
+    });*/
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    // Add '${implements OnChanges}' to the class.
+    this.usuarioLogueado = this.servUsrs.optenerUsuario();
+    console.log('cambios', this.servUsrs.optenerUsuario());
+
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBY3hmzVJSSKYPrtVOK3lqRip6wt8pZOyU",
+    authDomain: "palanaeumproyectouaa.firebaseapp.com",
+    databaseURL: "https://palanaeumproyectouaa.firebaseio.com",
+    projectId: "palanaeumproyectouaa",
+    storageBucket: "palanaeumproyectouaa.appspot.com",
+    messagingSenderId: "904432413605",
+    appId: "1:904432413605:web:f4f26536358f810038564c",
+    measurementId: "G-H0KE3D9ZZ4"
+  }
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
